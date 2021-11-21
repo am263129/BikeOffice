625f6d3fdfa9e99cefa246f99b3a31c919125ed8
Mufasa <orestoriga20119@gmail.com>
2021-11-22 04:37:45 +0900

    test 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
@@ -17,6 +17,24 @@
 
     <!-- React JS -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+</body>
+</html> --}}
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+    <h1>KILLLLl</h1>
 
 </body>
 </html>
